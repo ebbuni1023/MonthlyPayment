eb31264affa7cf03f135667ab4198b9a004ddd7b
ebbuni1023 <56244911+ebbuni1023@users.noreply.github.com>
2020-10-23 03:03:11 -0500

    Update Payment.c

--- Payment.c
+++ Payment.c
@@ -2,20 +2,25 @@
 
 int main(void)
 {
-	int amount_of_loan, monthly;
-	float payment, balance;
+   int monthly;
+   int amount_of_loan;
+   double payment, balance;
 
-	printf("Please input amount of loan: ");
-	scanf("%d", &amount_of_loan);
+   printf("Please input amount of loan: ");
+   scanf("%d", &amount_of_loan);
 
-	printf("Monthly\t\t\t Payment\t\t\t Balance");
+   printf("Monthly\t\t\t Payment\t\t Balance \n");
 
-	monthly = 12;
-	for (int i = 0; i < monthly, i++)
-	{	
-		payment = amount_of_loan / monthly;
-		balance = amount_of_loan - payment;
-		printf("%d\t\t\t %f.2\t\t\t %f.2\n", i, payment, balance);
-	}
-	return 0;
+   payment = amount_of_loan / 12.0;
+   balance = amount_of_loan;
+   //monthly = 12;
+   for (int i = 1; i <= monthly; i++)
+   {
+      monthly = 12;
+      balance = balance - payment;
+      if (balance < 0)
+         balance = -balance;
+      printf("%d\t\t\t %.2f\t\t\t %.2f\n", i, payment, balance);
+   }
+   return 0;
 }
